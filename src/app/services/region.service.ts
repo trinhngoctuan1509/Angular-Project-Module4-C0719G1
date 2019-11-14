01c08a98a1804ec4279b5dc6647c8c7489a2258b
Huynh Tan Dai <huynhtandai95@gmail.com>
2019-11-14 11:39:28 +0700

    update post list API and search API and add new service module of post,region,category

---
+++ src/app/services/region.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+@Injectable({
+  providedIn: 'root'
+})
+export class RegionService {
+  apiUrlRegion: string ="http://localhost:8000/api/region"
+  constructor(
+    private httpClient: HttpClient
+    ) { }
+    getAllRegions(): Observable<any> {
+      return this.httpClient.get(this.apiUrlRegion).pipe(map((response: any) => response));
+    }
+}

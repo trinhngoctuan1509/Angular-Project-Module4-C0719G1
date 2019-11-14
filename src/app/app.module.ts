7fd8e94f70defeb06ef51bcf39a4d46ab0f4c094
trinhngoctuan1509 <49947153+trinhngoctuan1509@users.noreply.github.com>
2019-11-14 09:43:05 +0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,7 @@ import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostSuccessComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,

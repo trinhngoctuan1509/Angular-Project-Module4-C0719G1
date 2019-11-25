079a49b242c10fcbd97b08fa5703ede8e524da6f
lamthevu <53170240+lamthevu@users.noreply.github.com>
2019-11-25 17:07:45 +0700

    update sprint2 lan 3

---
+++ src/app/components/Register/success-comfirm-mail/success-comfirm-mail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-comfirm-mail',
+  templateUrl: './success-comfirm-mail.component.html',
+  styleUrls: ['./success-comfirm-mail.component.css']
+})
+export class SuccessComfirmMailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

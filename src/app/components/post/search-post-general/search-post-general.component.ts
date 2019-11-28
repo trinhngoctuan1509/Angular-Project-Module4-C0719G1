c1be000d2b51ac72fa1a25308e483ef996f0e344
trinhngoctuan1509 <ngoctuan.qhvn@gmail.com>
2019-11-28 11:41:04 +0700

    su loi tim kiem nang cao routing

--- src/app/components/post/search-post-general/search-post-general.component.ts
+++ src/app/components/post/search-post-general/search-post-general.component.ts
@@ -29,7 +29,7 @@ export class SearchPostGeneralComponent implements OnInit {
       areaRangeId: ['0'],
       priceRangeId: ['0'],
       directionId: ['0'],
-      ableComposition: [false]
+      ableComposition: [true]
     })
   }
 
